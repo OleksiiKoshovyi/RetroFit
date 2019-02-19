5aba01fca66a4a33039b3d5be46a158c9ebb4cc8
pupil <alexeykoshevoi@gamil.com>
2019-02-19 15:44:29 +0200

    api

---
+++ app/src/main/java/com/example/fileloader/retrofit/AppAplication.java
@@ -0,0 +1,13 @@
+package com.example.fileloader.retrofit;
+
+import android.app.Application;
+import android.widget.Toast;
+
+public class AppAplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Toast.makeText(getBaseContext(),"There",Toast.LENGTH_SHORT).show();
+
+    }
+}
