9e27d7e207b6c0e86e164737105bb3b4afe6832e
pupil <alexeykoshevoi@gamil.com>
2019-02-19 15:46:56 +0200

    api

--- app/src/main/java/com/example/fileloader/retrofit/Service.java
+++ app/src/main/java/com/example/fileloader/retrofit/Service.java
@@ -1,13 +1,14 @@
 package com.example.fileloader.retrofit;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Service {
 
     public static API getApi(){
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://www.recipepuppy.com/api/")
-//                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
                 .build();
         API api = retrofit.create(API.class);
         return  api;
