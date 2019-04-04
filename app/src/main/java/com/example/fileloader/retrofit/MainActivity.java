53c843c80cd9fc975dd3871f391c32b4e631d5b0
nebosky142 <alexeykoshevoi@gmail.com>
2019-04-04 21:44:47 +0300

    import picasso for downloading pictures

--- app/src/main/java/com/example/fileloader/retrofit/MainActivity.java
+++ app/src/main/java/com/example/fileloader/retrofit/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.fileloader.retrofit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -9,25 +15,39 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity implements Callback<DataModel> {
 
+    RecyclerView rview;
+
+    List<Result> results = new ArrayList<>();
+    RecipeAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rview = findViewById(R.id.rview);
+        rview.setLayoutManager(new LinearLayoutManager(this));
 
-        getRecipe();
+        adapter = new RecipeAdapter(results, this);
+        rview.setAdapter(adapter);
+
+       getRecipe();
     }
 
     private void getRecipe(){
         Call<DataModel> call = Service.getApi().getRecipe();
+
         call.enqueue(new Callback<DataModel>() {
             @Override
             public void onResponse(Call<DataModel> call, Response<DataModel> response) {
-                response.body().toString();
+                //response.body().toString();
+                results = response.body().getResults();
+                adapter.setList(results);
             }
 
             @Override
             public void onFailure(Call<DataModel> call, Throwable t) {
                 t.getMessage();
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
@@ -43,6 +63,3 @@ public class MainActivity extends AppCompatActivity implements Callback<DataMode
 
     }
 }
-/*
-    Не трожьте этот проект
-* */
--- app/src/main/java/com/example/fileloader/retrofit/RecipeAdapter.java
+++ app/src/main/java/com/example/fileloader/retrofit/RecipeAdapter.java
@@ -1,21 +1,49 @@
 package com.example.fileloader.retrofit;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static java.security.AccessController.getContext;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
     TextView txtTitle,txtIngredients,txtUrl;
+    ImageView img;
+    Context context;
     private List<Result> results;
 
-    public RecipeAdapter(List<Result> results) {
+    public RecipeAdapter(List<Result> results, Context context) {
         this.results = results;
+        this.context = context;
     }
+
+    public void setList(List<Result> results)
+    {
+        this.results = results;
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public RecipeAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
@@ -38,9 +66,20 @@ public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder
             txtTitle = itemView.findViewById(R.id.txtTitle);
             txtIngredients = itemView.findViewById(R.id.txtIngredients);
             txtUrl = itemView.findViewById(R.id.txtUrl);
+            img = itemView.findViewById(R.id.img);
         }
 
         public void onBind(Result result){
+            txtTitle.setText(String.valueOf(result.getTitle()));
+            txtIngredients.setText(String.valueOf(result.getIngredients()));
+            txtUrl.setText(String.valueOf(result.getHref()));
+
+            Picasso.with(context)
+                    .load(result.getThumbnail())
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_foreground)
+                    .into(img);
         }
     }
+
 }
